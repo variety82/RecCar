84da9dc16d86e5515428c3f5dd854e5308976e8e
changhyunKim <ch03124@naver.com>
2023-03-13 16:43:17 +0900

    feat : 차량 파손정보 입력 서비스 구현

--- BE/src/main/java/com/heros/api/detectionInfo/service/DetectionInfoService.java
+++ BE/src/main/java/com/heros/api/detectionInfo/service/DetectionInfoService.java
@@ -1,7 +1,11 @@
 package com.heros.api.detectionInfo.service;
 
+import com.heros.api.car.entity.Car;
+import com.heros.api.car.repository.CarRepository;
+import com.heros.api.detectionInfo.dto.request.DetectionInfoCreate;
 import com.heros.api.detectionInfo.dto.response.PartWithDetectionInfoResponse;
 import com.heros.api.detectionInfo.dto.response.RentDetailResponse;
+import com.heros.api.detectionInfo.entity.DetectionInfo;
 import com.heros.api.detectionInfo.repository.DetectionInfoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class DetectionInfoService {
 
     private final DetectionInfoRepository detectionInfoRepository;
+    private final CarRepository carRepository;
     public PartWithDetectionInfoResponse getDetectionInfos(Long carId){
         PartWithDetectionInfoResponse detectionInfos = detectionInfoRepository.getDetectionInfos(carId);
         return detectionInfos;
@@ -24,5 +29,20 @@ public class DetectionInfoService {
     public RentDetailResponse getRentDetailInfos(Long carId){
         RentDetailResponse rentDetailInfos = detectionInfoRepository.getRentalDetailInfos(carId);
         return rentDetailInfos;
+    }
+
+    @Transactional
+    public void createDamageInfo(DetectionInfoCreate detectionInfoCreate){
+        Car car = carRepository.findById(detectionInfoCreate.getCarId()).orElseThrow(IllegalArgumentException::new);
+        DetectionInfo detectionInfo =  DetectionInfo.builder()
+                .car(car)
+                .former(detectionInfoCreate.isFormer())
+                .damageImageUrl(detectionInfoCreate.getPictureUrl())
+                .part(detectionInfoCreate.getPart())
+                .damage(detectionInfoCreate.getDamage())
+                .memo(detectionInfoCreate.getMemo())
+                .damageDate(detectionInfoCreate.getDamageDate())
+                .build();
+        detectionInfoRepository.save(detectionInfo);
     }
 }
