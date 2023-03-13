5b755c5e5ebc9e1a65dd1fd2d35ddc215d2a6c53
changhyunKim <ch03124@naver.com>
2023-03-13 15:59:31 +0900

    feat : 렌탈상세정보 Repo 구현

--- BE/src/main/java/com/heros/api/detectionInfo/repository/DetectionInfoRepositoryImpl.java
+++ BE/src/main/java/com/heros/api/detectionInfo/repository/DetectionInfoRepositoryImpl.java
@@ -1,12 +1,17 @@
 package com.heros.api.detectionInfo.repository;
 
 import com.heros.api.detectionInfo.dto.response.PartWithDetectionInfoResponse;
+import com.heros.api.detectionInfo.dto.response.RentDetailResponse;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 
+import java.util.List;
+
+import static com.heros.api.car.entity.QCar.car;
 import static com.heros.api.detectionInfo.entity.QDetectionInfo.detectionInfo;
 
 @Slf4j
@@ -17,6 +22,7 @@ public class DetectionInfoRepositoryImpl implements DetectionInfoRepositorySuppo
     public PartWithDetectionInfoResponse getDetectionInfos(Long carId){
         PartWithDetectionInfoResponse result = queryFactory
                 .select(Projections.constructor(PartWithDetectionInfoResponse.class,
+                        detectionInfo.part,
                         detectionInfo.damage,
                         detectionInfo.damageDate,
                         detectionInfo.damageImageUrl,
@@ -25,6 +31,31 @@ public class DetectionInfoRepositoryImpl implements DetectionInfoRepositorySuppo
                 .where(detectionInfo.car.carId.eq(carId))
                 .fetchOne();
         log.info("DetectionInfo : {result}");
+        return result;
+    }
+
+
+    @Override
+    public RentDetailResponse getRentalDetailInfos(Long carId) {
+        RentDetailResponse result = queryFactory
+                .select(Projections.constructor(RentDetailResponse.class,
+                        car.carId,
+                        car.carNumber,
+                        car.carManufacturer,
+                        car.carModel,
+                        car.carFuel,
+                        car.rentalDate,
+                        car.returnDate,
+                        car.rentalCompany,
+                        detectionInfo.part,
+                        detectionInfo.damage,
+                        detectionInfo.damageDate,
+                        detectionInfo.damageImageUrl,
+                        detectionInfo.memo))
+                .from(detectionInfo)
+                .where(detectionInfo.car.carId.eq(carId))
+                .fetchOne();
+        log.info("RentalDetail : {result}");
         return result;
     }
 }
