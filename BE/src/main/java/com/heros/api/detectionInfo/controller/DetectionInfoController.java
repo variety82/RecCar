9f49d002e0ec951798b44eb7527a49e33df59dfb
changhyunKim <ch03124@naver.com>
2023-03-13 16:43:39 +0900

    feat : 차량 파손정보 입력 API 구현

--- BE/src/main/java/com/heros/api/detectionInfo/controller/DetectionInfoController.java
+++ BE/src/main/java/com/heros/api/detectionInfo/controller/DetectionInfoController.java
@@ -1,6 +1,7 @@
 package com.heros.api.detectionInfo.controller;
 
 
+import com.heros.api.detectionInfo.dto.request.DetectionInfoCreate;
 import com.heros.api.detectionInfo.dto.response.PartWithDetectionInfoResponse;
 import com.heros.api.detectionInfo.dto.response.RentDetailResponse;
 import com.heros.api.detectionInfo.service.DetectionInfoService;
@@ -16,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.EntityResponse;
 
@@ -49,5 +52,16 @@ public class DetectionInfoController {
         RentDetailResponse rentalDetailInfos = detectionInfoService.getRentDetailInfos(carId);
 
         return ResponseEntity.ok().body(rentalDetailInfos);
+    }
+
+    @Operation(summary = "파손 정보 등록")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success", content = @Content(schema = @Schema(implementation = DetectionInfoCreate.class))),
+            @ApiResponse(responseCode = "404", description = "fail", content = @Content(schema = @Schema(implementation = ErrorResponseExample.class)))
+    })
+    @PostMapping(value = "/api/v1/detection")
+    public ResponseEntity<?> login(@RequestBody DetectionInfoCreate detectionInfoCreate) {
+        detectionInfoService.createDamageInfo(detectionInfoCreate);
+        return ResponseEntity.status(201).body(null);
     }
 }
