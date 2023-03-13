ed4b54a4b226a94e1a20eb42dd78b038ae658db3
changhyunKim <ch03124@naver.com>
2023-03-13 16:00:01 +0900

    feat : 렌탈상세정보 API 구현

--- BE/src/main/java/com/heros/api/detectionInfo/controller/DetectionInfoController.java
+++ BE/src/main/java/com/heros/api/detectionInfo/controller/DetectionInfoController.java
@@ -2,6 +2,7 @@ package com.heros.api.detectionInfo.controller;
 
 
 import com.heros.api.detectionInfo.dto.response.PartWithDetectionInfoResponse;
+import com.heros.api.detectionInfo.dto.response.RentDetailResponse;
 import com.heros.api.detectionInfo.service.DetectionInfoService;
 import com.heros.api.example.model.ErrorResponseExample;
 import com.heros.api.example.model.UserValue;
@@ -13,8 +14,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
 
 @Tag(name = "DetectionInfoController", description = "차량 파손 정보 API")
 @RestController
@@ -29,10 +32,22 @@ public class DetectionInfoController {
             @ApiResponse(responseCode = "404", description = "fail", content = @Content(schema = @Schema(implementation = ErrorResponseExample.class)))
     })
     @GetMapping(value = "/api/v1/detection")
-    public PartWithDetectionInfoResponse getDetectionInfos(@Param(value = "carId") Long carId){
+    public ResponseEntity<?> getDetectionInfos(@Param(value = "carId") Long carId){
 
         PartWithDetectionInfoResponse detectionInfos = detectionInfoService.getDetectionInfos(carId);
 
-        return detectionInfos;
+        return ResponseEntity.ok().body(detectionInfos);
+    }
+
+    @Operation(summary = "렌트 내역 상세 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success", content = @Content(schema = @Schema(implementation = RentDetailResponse.class))),
+            @ApiResponse(responseCode = "404", description = "fail", content = @Content(schema = @Schema(implementation = ErrorResponseExample.class)))
+    })
+    @GetMapping(value = "/api/v1/detection/rental")
+    public ResponseEntity<?> getRentDetailInfos(@Param(value = "carId") Long carId){
+        RentDetailResponse rentalDetailInfos = detectionInfoService.getRentDetailInfos(carId);
+
+        return ResponseEntity.ok().body(rentalDetailInfos);
     }
 }
