6f2946d605d1b1872daaff33b9d566a7ce7ca8c8
임두현 <dhlim07@naver.com>
2023-03-14 16:18:37 +0900

    feat : Set select logic in maker select page

--- client/lib/screens/register/select_maker.dart
+++ client/lib/screens/register/select_maker.dart
@@ -2,10 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:client/widgets/register/category_title.dart';
 import 'package:client/widgets/register/maker_item.dart';
 
-class SelectMaker extends StatelessWidget {
+class SelectMaker extends StatefulWidget {
   const SelectMaker({
     super.key,
   });
+
+  @override
+  State<SelectMaker> createState() => _SelectMakerState();
+}
+
+class _SelectMakerState extends State<SelectMaker> {
+  /*
+  selectedItem에는 선택되는 제조사의 id가 들어간다
+  isSelected는 만약 makerId가 selectedItem이 같을 때 true가 되도록 한다.
+   */
+
+  int? selectedItem;
+  void _changeSelectedItem(itemId) {
+    setState(() {
+      if (selectedItem == itemId) {
+        selectedItem = null;
+      } else {
+        selectedItem = itemId;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,15 +69,25 @@ class SelectMaker extends StatelessWidget {
           height: 1,
           color: const Color(0xFFEFEFEF),
         ),
-        const SizedBox(
+        Container(
           width: double.infinity,
           height: 50,
-          child: Center(
-            child: Text('제조사를 선택해주세요.'),
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 30,
+          ) ,
+          child: Text(
+            '제조사를 선택해주세요.',
+            style: TextStyle(
+              color: Theme.of(context).secondaryHeaderColor,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
-        const Padding(
-          padding: EdgeInsets.all(20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: SizedBox(
             width: double.infinity,
             child: Wrap(
@@ -64,10 +95,16 @@ class SelectMaker extends StatelessWidget {
               direction: Axis.horizontal,
               children: [
                 MakerItem(
+                  changeSelectedItem: _changeSelectedItem,
+                  isSelected: selectedItem == 1 ? true : false,
+                  makerId: 1,
                   makerTitle: '싸피',
                   makerImageUrl: 'https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/71140183_2565406580147654_6224942049100038144_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=rFRCaBYqX_kAX-6Pv-x&_nc_ht=scontent-ssn1-1.xx&oh=00_AfA_fpZatlF37xtx_gAEwJunxUpppE_QUaVmozu5c9EURA&oe=64364939',
                 ),
                 MakerItem(
+                  changeSelectedItem: _changeSelectedItem,
+                  isSelected: selectedItem == 2 ? true : false,
+                  makerId: 2,
                   makerTitle: '싸피',
                   makerImageUrl: 'https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/71140183_2565406580147654_6224942049100038144_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=rFRCaBYqX_kAX-6Pv-x&_nc_ht=scontent-ssn1-1.xx&oh=00_AfA_fpZatlF37xtx_gAEwJunxUpppE_QUaVmozu5c9EURA&oe=64364939',
                 ),
