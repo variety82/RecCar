1601bb1dd844ff148c0b3e6389e74d884f371275
임두현 <dhlim07@naver.com>
2023-03-14 17:44:22 +0900

    feat : Update select logic in select maker

--- client/lib/screens/register/select_maker.dart
+++ client/lib/screens/register/select_maker.dart
@@ -16,14 +16,17 @@ class _SelectMakerState extends State<SelectMaker> {
   selectedItem에는 선택되는 제조사의 id가 들어간다
   isSelected는 만약 makerId가 selectedItem이 같을 때 true가 되도록 한다.
    */
-
-  int? selectedItem;
+  
+  // 선택된 제조사의 ID 값이 들어간다
+  int? selectedMaker;
   void _changeSelectedItem(itemId) {
     setState(() {
-      if (selectedItem == itemId) {
-        selectedItem = null;
+      if (selectedMaker == itemId) {
+        // 선택한 제조사가 현재 선택된 제조사일 경우 null값으로 변경
+        selectedMaker = null;
       } else {
-        selectedItem = itemId;
+        // 다를 경우 해당 제조사 ID로 selectedMaker 변경
+        selectedMaker = itemId;
       }
     });
   }
@@ -32,6 +35,7 @@ class _SelectMakerState extends State<SelectMaker> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // Modal Bar
         Container(
           margin: const EdgeInsets.only(top: 2),
           width: 50,
@@ -46,6 +50,7 @@ class _SelectMakerState extends State<SelectMaker> {
               vertical: 8,
               horizontal: 20
           ),
+          // 카테고리 리스트, 현재는 선택해서 이동 불가하고 시간 남으면 클릭시 모달 내용 변경하도록 설정
           child: Row(
             children: [
               categoryTitle(
@@ -63,12 +68,14 @@ class _SelectMakerState extends State<SelectMaker> {
             ],
           ),
         ),
+        // 카테고리 하단 바
         Container(
           margin: const EdgeInsets.only(top: 5),
           width: double.infinity,
           height: 1,
           color: const Color(0xFFEFEFEF),
         ),
+        // 제조사 선택 Body의 제목
         Container(
           width: double.infinity,
           height: 50,
@@ -86,6 +93,7 @@ class _SelectMakerState extends State<SelectMaker> {
             ),
           ),
         ),
+        // 제조사 선택 영역이 나열될 곳
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: SizedBox(
@@ -96,14 +104,14 @@ class _SelectMakerState extends State<SelectMaker> {
               children: [
                 MakerItem(
                   changeSelectedItem: _changeSelectedItem,
-                  isSelected: selectedItem == 1 ? true : false,
+                  isSelected: selectedMaker == 1 ? true : false,
                   makerId: 1,
                   makerTitle: '싸피',
                   makerImageUrl: 'https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/71140183_2565406580147654_6224942049100038144_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=rFRCaBYqX_kAX-6Pv-x&_nc_ht=scontent-ssn1-1.xx&oh=00_AfA_fpZatlF37xtx_gAEwJunxUpppE_QUaVmozu5c9EURA&oe=64364939',
                 ),
                 MakerItem(
                   changeSelectedItem: _changeSelectedItem,
-                  isSelected: selectedItem == 2 ? true : false,
+                  isSelected: selectedMaker == 2 ? true : false,
                   makerId: 2,
                   makerTitle: '싸피',
                   makerImageUrl: 'https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/71140183_2565406580147654_6224942049100038144_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=rFRCaBYqX_kAX-6Pv-x&_nc_ht=scontent-ssn1-1.xx&oh=00_AfA_fpZatlF37xtx_gAEwJunxUpppE_QUaVmozu5c9EURA&oe=64364939',
