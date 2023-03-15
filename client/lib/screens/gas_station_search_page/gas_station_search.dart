b80e0b624c89b846fffa14bda32d3c87f36a0603
songheewon <songheew1020@gmail.com>
2023-03-15 14:17:58 +0900

    feat : 지도 검색 및 지도 이동 완성

--- client/lib/screens/gas_station_search_page/gas_station_search.dart
+++ client/lib/screens/gas_station_search_page/gas_station_search.dart
@@ -6,7 +6,7 @@ import '../../widgets/common/footer.dart';
 import 'package:draggable_bottom_sheet/draggable_bottom_sheet.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
-import 'package:geolocator/geolocator.dart';
+// import 'package:geolocator/geolocator.dart';
 
 class NaverMapTest extends StatefulWidget {
   @override
@@ -15,23 +15,18 @@ class NaverMapTest extends StatefulWidget {
 
 class _NaverMapTestState extends State<NaverMapTest> {
   naver.MapType _mapType = naver.MapType.Basic;
-  naver.CameraPosition _cameraPosition =
-      naver.CameraPosition(target: naver.LatLng(37.2, 23));
   late naver.NaverMapController _controller;
   List<Map<String, dynamic>> result =
       List<Map<String, dynamic>>.filled(100, {});
-  // late Geolocator _geolocator;
-  // late Position _position;
-
-  // final keywordController = TextEditingController();
+  bool beforeSearch = true;
 
   Widget _previewWidget() {
     return Container(
       decoration: const BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
         ),
       ),
       child: Column(
@@ -43,14 +38,7 @@ class _NaverMapTestState extends State<NaverMapTest> {
             thickness: 2,
             height: 30,
           ),
-          Container(
-              // width: 40,
-              // height: 6,
-              // decoration: BoxDecoration(
-              //   color: Colors.white,
-              //   borderRadius: BorderRadius.circular(10),
-              // ),
-              ),
+          Container(),
           const SizedBox(height: 16),
         ],
       ),
@@ -59,7 +47,6 @@ class _NaverMapTestState extends State<NaverMapTest> {
 
   Widget _expandedWidget() {
     return Container(
-      // padding: const EdgeInsets.all(16),
       decoration: const BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.only(
@@ -68,24 +55,21 @@ class _NaverMapTestState extends State<NaverMapTest> {
         ),
       ),
       child: Column(
-        children: <Widget>[
-          const Icon(Icons.keyboard_arrow_down, size: 30, color: Colors.white),
-          // const SizedBox(height: 8),
-          const Text(
-            "검색 결과",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.none,
-            ),
+        children: [
+          Divider(
+            color: Color(0xFF6A6A6A),
+            indent: 160,
+            endIndent: 160,
+            thickness: 2,
+            height: 30,
           ),
-          const SizedBox(height: 3),
+          const SizedBox(height: 10),
           Expanded(
             child: SingleChildScrollView(
               child: Container(
                 width: 800,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     if (result[0].isEmpty)
                       Text(
@@ -102,14 +86,59 @@ class _NaverMapTestState extends State<NaverMapTest> {
                         if (result[i] != {})
                           TextButton(
                             onPressed: () => move(i),
-                            child: Text(
-                              "${result[i]['name']}",
-                              style: TextStyle(
-                                color: Colors.black,
-                                decoration: TextDecoration.none,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                height: 3,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Color(0xFFD9D9D9),
+                                  ),
+                                ),
+                              ),
+                              padding: EdgeInsetsDirectional.only(
+                                bottom: 10,
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 5,
+                                      ),
+                                      child: Text(
+                                        "${result[i]['name']}",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          decoration: TextDecoration.none,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          // height: 2,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 5,
+                                      ),
+                                      child: Text(
+                                        "${result[i]['upperAddrName']} ${result[i]['middleAddrName']} ${result[i]['lowerAddrName']}",
+                                        style: TextStyle(
+                                          // color: Colors.black,
+                                          // decoration: TextDecoration.none,
+                                          // fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          // height: 2,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
@@ -147,7 +176,8 @@ class _NaverMapTestState extends State<NaverMapTest> {
                     child: TextField(
                       decoration: InputDecoration(
                         border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30)),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                         labelText: '검색어',
                       ),
                       onSubmitted: (value) => search(value),
@@ -172,13 +202,23 @@ class _NaverMapTestState extends State<NaverMapTest> {
         Expanded(
           child: Stack(
             children: [
-              DraggableBottomSheet(
-                previewWidget: _previewWidget(),
-                backgroundWidget: _backgroundWidget(),
-                expandedWidget: _expandedWidget(),
-                onDragging: (pos) {},
-                maxExtent: 400,
-              ),
+              if (beforeSearch)
+                DraggableBottomSheet(
+                  previewWidget: _previewWidget(),
+                  backgroundWidget: _backgroundWidget(),
+                  expandedWidget: _expandedWidget(),
+                  onDragging: (pos) {},
+                  maxExtent: 400,
+                ),
+              if (!beforeSearch)
+                DraggableBottomSheet(
+                  previewWidget: _expandedWidget(),
+                  backgroundWidget: _backgroundWidget(),
+                  expandedWidget: _expandedWidget(),
+                  onDragging: (pos) {},
+                  maxExtent: 400,
+                  collapsed: false,
+                ),
             ],
           ),
         ),
@@ -192,9 +232,10 @@ class _NaverMapTestState extends State<NaverMapTest> {
       _controller = controller;
     });
   }
-  //
+
   // void onMapTap(naver.LatLng latLng) {
-  //   _geolocator = Geolocator();
+  //   search("주유소");
+  //   print("id: " + result[0]['id']);
   // }
 
   void move(int i) {
@@ -203,10 +244,29 @@ class _NaverMapTestState extends State<NaverMapTest> {
     var latLng = naver.LatLng(lat, lng);
     _controller.moveCamera(naver.CameraUpdate.toCameraPosition(
         naver.CameraPosition(target: latLng)));
-    naver.Marker(markerId: 'marker1', position: latLng);
+    naver.Marker(
+        markerId: "${result[i]["name"]}",
+        position: latLng,
+        iconTintColor: Colors.black);
+    // if(result[i]["name"].toString().contains("주유소")) { // 주유소의 경우 유가 정보 출력해주려했는데
+    //   getGasInfo(result[i]["id"]);
+    // }
   }
 
-
+  // Future<void> getGasInfo(id) async { // 유가 정보 불러오는 API
+  //   Map<String, String> headers = {
+  //     "appkey": "l7xxe210feb29ba24deda6a06b0d0e88366a",
+  //   };
+  //   Response response = await get(
+  //     Uri.parse(
+  //         "https://apis.openapi.sk.com/tmap/oilinfo/poiDetailOil?version=1&poiId=${id}"),
+  //     headers: headers,
+  //   );
+  //   var jsonData = response.body;
+  //   var ret = jsonDecode(jsonData);
+  //   print(ret);
+  //   // 유가 정보 API 이용은 1일 1회만 무료래용......
+  // }
 
   Future<void> search(keyword) async {
     Map<String, String> headers = {
@@ -218,7 +278,6 @@ class _NaverMapTestState extends State<NaverMapTest> {
       headers: headers,
     );
     var jsonData = response.body;
-    // print(jsonData['searchPoiInfo']);
     var ret = jsonDecode(jsonData)["searchPoiInfo"]['pois']['poi'];
 
     setState(() {
