8cd0f94d95e2cb127ccdd6108d35b0f2e42b9b6a
임두현 <dhlim07@naver.com>
2023-03-21 17:56:23 +0900

    feat : Update car detail page

--- client/lib/screens/car_detail/car_detail.dart
+++ client/lib/screens/car_detail/car_detail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:client/widgets/common/header.dart';
 import 'package:client/widgets/common/footer.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:client/widgets/detail/damage_level_card.dart';
 
 
 class CarDetail extends StatefulWidget {
@@ -13,7 +14,7 @@ class CarDetail extends StatefulWidget {
 
 class _CarDetailState extends State<CarDetail> {
 
-  int sideDamageLevel = 3;
+  int sideDamageLevel = 2;
   int frontDamageLevel = 1;
   int backDamageLevel = 2;
   int wheelDamageLevel = 0;
@@ -25,7 +26,7 @@ class _CarDetailState extends State<CarDetail> {
       body: Column(
         children: [
           const Header(
-            title: '차량 상세'
+            title: '차량 상세 정보'
           ),
           Expanded(child: Padding(
             padding: const EdgeInsets.all(8.0),
@@ -34,20 +35,45 @@ class _CarDetailState extends State<CarDetail> {
                 const SizedBox(
                   height: 50,
                 ),
+                Text(
+                  'SM5',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).secondaryHeaderColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
                 SvgPicture.asset(
                   'lib/assets/car_damage_img/car_f${frontDamageLevel}_s${sideDamageLevel}_b${backDamageLevel}_w$wheelDamageLevel.svg',
                 ),
-                Text('측면 $sideDamageLevel건'),
-                Text('전면부 $frontDamageLevel건'),
-                Text('후면부 $backDamageLevel건'),
-                Text('타이어 $wheelDamageLevel건'),
+                const SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '손상 부위',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).secondaryHeaderColor,
+                    ),
+                  ),
+                ),
+                DamageLevelCard(sideDamageLevel: sideDamageLevel),
 
-              ]
+              ],
             ),
-          )),
+          ),
+          ),
           const Footer()
         ],
       )
     );
   }
 }
+
+
