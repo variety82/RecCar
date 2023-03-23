2f65cfd27167ffc355a5f34faebb8ad969a2ac82
Taekyun Kim <taekyun.contact@gmail.com>
2023-03-23 17:26:42 +0900

    feat : detectionInfo findById 추가

--- BE/src/main/java/com/heros/api/detectionInfo/repository/DetectionInfoRepository.java
+++ BE/src/main/java/com/heros/api/detectionInfo/repository/DetectionInfoRepository.java
@@ -3,5 +3,9 @@ package com.heros.api.detectionInfo.repository;
 import com.heros.api.detectionInfo.entity.DetectionInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DetectionInfoRepository extends JpaRepository<DetectionInfo, Long>, DetectionInfoRepositorySupport {
+    @Override
+    Optional<DetectionInfo> findById(Long aLong);
 }
