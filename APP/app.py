18e51823bd657fb198c3e503858f21522c0db89c
changhyunKim <ch03124@naver.com>
2023-03-23 14:29:34 +0900

    refactor : API 속도 개선

--- APP/app.py
+++ APP/app.py
@@ -5,11 +5,12 @@ import datetime
 import inference
 from service.s3_service import *
 from service.video_service import *
+import time
 
 app = FastAPI()
 
-s3 = s3_connection()
-bucket = os.getenv("bucket_name")
+# s3 = s3_connection()
+# bucket = os.getenv("bucket_name")
 
 origins = [
     "http://localhost:3000"
@@ -23,23 +24,52 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.on_event("startup")
+def startup_event():
+    global s3, bucket, model
+    s3 = s3_connection()
+    bucket = os.getenv("bucket_name")
+    model = inference.load_model()
+
 @app.post("/ai-api/v1/damage") 
 async def upload_video(file : UploadFile, user_id : str):
+    before = time.time()
     upload_path = "./dataset/video"
     content = await file.read()
     now = str(datetime.datetime.now()).replace(" ", "_").replace(":", "-")[:-7]
+    # 비디오 to 이미지
+    vidoe_to_image_before = time.time()
 
     filen_name = f"user_{user_id}_{now}.mp4"
     with open(os.path.join(upload_path, filen_name), "wb") as fp:
         fp.write(content)  # 서버 로컬 스토리지에 이미지 저장 (쓰기)
 
     images = video_to_image(filen_name)
-    created_img = inference.create_images(images)
+    vidoe_to_image_after = time.time()
 
+    # print(f"video_to_image : {vidoe_to_image_after - vidoe_to_image_before}")
+
+    # print(f"create_images : {images}")
+
+    inference_before = time.time()
+    created_img = inference.create_images(model, images)
+    inference_after = time.time()
+
+    print(f"inference time : {inference_after - inference_before}")
+
+    s3_before = time.time()
     obj_list = upload_s3(s3, bucket, created_img, "hero")
+    # print(f"created_img : {created_img}")
     images_list = get_images_url(s3, bucket, obj_list)
+    s3_after = time.time()
+
+    # print(f"s3 time : {s3_after - s3_before}")
+
+    after = time.time()
+    # print(f"total time : {after - before}")
 
     return images_list
 
 if __name__ == '__main__':
-    uvicorn.run(app, host = '0.0.0.0', port=8081)
+    uvicorn.run("app:app", reload = True, host = '0.0.0.0', port=8081)
--- APP/inference.py
+++ APP/inference.py
@@ -1,8 +1,7 @@
 import time
-before = time.time()
-
 import torch 
 import cv2
+import os
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
@@ -14,8 +13,8 @@ n_classes = 2
 
 
 def load_model():
+    #Scratch만 
     weight_path = './models/[DAMAGE][Scratch_0]Unet.pt'
-
     #Load pretrained model
     model = Unet(encoder='resnet34', pre_weight='imagenet', num_classes=n_classes).to(device)
     model.model.load_state_dict(torch.load(weight_path, map_location=torch.device(device)))
@@ -26,10 +25,10 @@ def load_img(img_name):
     # load img
     #org_img : 원본이미지
     #img_img : 인퍼런스하기 위해 정규화 및 RGB변경
-    img_path = f"./images/{img_name}"
+    img_path = f"./dataset/images/{img_name}"
     org_img  = cv2.imread(img_path)
     org_img = cv2.cvtColor(org_img, cv2.COLOR_BGR2RGB)
-    org_img = cv2.resize(org_img, (256, 256))
+    org_img = cv2.resize(org_img, (512, 512))
 
     img_input = org_img / 255.
     img_input = img_input.transpose([2, 0, 1])
@@ -41,7 +40,13 @@ def load_img(img_name):
 
 
 def inference_img(model, org_img, img_input, output_name):
+
+    model_before = time.time()
     output = model(img_input)
+    model_after = time.time()
+    print(f"model time : {model_after - model_before}")
+
+    image_save_before = time.time()
     img_output = torch.argmax(output, dim=1).detach().cpu().numpy()
     img_output = img_output.transpose([1, 2, 0])
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 10))
@@ -55,23 +60,30 @@ def inference_img(model, org_img, img_input, output_name):
     ax[1].set_title('damage')
     ax[1].axis('off')
     fig.set_tight_layout(True)
-    plt.savefig(f'./output_images/{output_name}', dpi=100)
+
+    if not os.path.exists("./dataset/output_images"):
+            os.makedirs("./dataset/output_images")
+
+    image_save_after = time.time()
+    print(f"image_save_time : {image_save_after - image_save_before}")
+
+
+    # 얘가 오래걸림 ;
+    plt.savefig(f'./dataset/output_images/{output_name}', dpi=50)
 
     return output_name
 
-def create_images(images):
-    model = load_model()
+def create_images(model, images):
+
     created_images = []
     for img in images:
         org_img, img_input = load_img(img)
+
+        inference_before = time.time()
         output_img = inference_img(model, org_img, img_input, f"damage_{img}")
+        inference_after = time.time()
+
+        print(f"inference time : {inference_after -inference_before}")
         created_images.append(output_img)
 
     return created_images
-
-images = ["t1.jpg", "t2.jpg"]
-
-created_img = create_images(images)
-# after = time.time()
-
-# print(after - before)
