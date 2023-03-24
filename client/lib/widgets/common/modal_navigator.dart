d91e6950ecd9f3eda68372eecc5acd80161b007f
임두현 <dhlim07@naver.com>
2023-03-24 15:49:29 +0900

    feat : Add modal disable

--- client/lib/widgets/common/modal_navigator.dart
+++ client/lib/widgets/common/modal_navigator.dart
@@ -4,34 +4,38 @@ class ModalNavigator extends StatelessWidget {
 
   final Widget showedWidget;
   final Widget child;
+  final bool disable;
 
   const ModalNavigator({
-    super.key, required this.showedWidget, required this.child,
+    super.key, required this.showedWidget, required this.child, required this.disable,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        showModalBottomSheet(
-          context: context,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(25.0),
-            ),
-          ),
-          builder: (BuildContext context) {
-            return Card(
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(25.0),
-                  ),
+      onTap:
+        disable
+          ? () {}
+          : () {
+            showModalBottomSheet(
+              context: context,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(25.0),
                 ),
-                child: showedWidget
+              ),
+              builder: (BuildContext context) {
+                return Card(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(25.0),
+                      ),
+                    ),
+                    child: showedWidget
+                );
+              },
             );
           },
-        );
-      },
       child: child
     );
   }
