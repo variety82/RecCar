1165c89560f0ce3f6c0b4f470f2a9aeb80bb65f8
Taekyun Kim <taekyun.contact@gmail.com>
2023-03-27 14:30:33 +0900

    feat : CalendarRepository 추가

---
+++ BE/src/main/java/com/heros/api/calendar/repository/CalendarRepository.java
@@ -0,0 +1,7 @@
+package com.heros.api.calendar.repository;
+
+import com.heros.api.calendar.entity.Calendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalendarRepository extends JpaRepository<Calendar, Long>, CalendarRepositorySupport  {
+}
