9d6e46c4b6e6653591bcdd9299d45599ca8f363c
Taekyun Kim <taekyun.contact@gmail.com>
2023-03-27 15:42:01 +0900

    feat : CalendarRepository deleteById 추가

--- BE/src/main/java/com/heros/api/calendar/repository/CalendarRepository.java
+++ BE/src/main/java/com/heros/api/calendar/repository/CalendarRepository.java
@@ -4,4 +4,6 @@ import com.heros.api.calendar.entity.Calendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CalendarRepository extends JpaRepository<Calendar, Long>, CalendarRepositorySupport  {
+    @Override
+    void deleteById(Long calendarId);
 }
