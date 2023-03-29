30e090ec1d209b9bdeadbd2198dd0bdfdc897c6a
changhyunKim <ch03124@naver.com>
2023-03-29 14:19:36 +0900

    feat : model 추가및 스키마 변경

--- APP/app.py
+++ APP/app.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
-import datetime
-import inference
+from typing import List
 from service.s3_service import *
 from service.video_service import *
+import inference
+import datetime
+import schema
+import uvicorn
 import time
 
 app = FastAPI()
-
-# s3 = s3_connection()
-# bucket = os.getenv("bucket_name")
 
 origins = [
     "http://localhost:3000"
@@ -24,17 +23,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# labels = ['Scratch', 'Crushed', 'Breakage']
+labels = ['Scratch_0', 'Breakage_3', 'Crushed_2']
 
 @app.on_event("startup")
 def startup_event():
-    global s3, bucket, model
+    global s3, bucket, models
     s3 = s3_connection()
     bucket = os.getenv("bucket_name")
-    model = inference.load_model()
+    models = inference.load_model(labels)
 
 @app.post("/ai-api/v1/damage") 
-async def upload_video(file : UploadFile, user_id : str):
+async def upload_video(file : UploadFile, user_id : str, response_model=schema.Inference_image):
     upload_path = "./dataset/video"
     content = await file.read()
     now = str(datetime.datetime.now()).replace(" ", "_").replace(":", "-")[:-7]
@@ -43,14 +42,20 @@ async def upload_video(file : UploadFile, user_id : str):
     with open(os.path.join(upload_path, filen_name), "wb") as fp:
         fp.write(content)  # 서버 로컬 스토리지에 이미지 저장 (쓰기)
     
-    images = video_to_image(filen_name, 60)
-    
-    created_img = inference.create_images(model, images)
+    images = video_to_image(filen_name, 30)
+
+    created_img = inference.create_images(models, images)
 
     obj_list = upload_s3(s3, bucket, created_img, "hero")
     images_list = get_images_url(s3, bucket, obj_list)
 
-    return images_list
+    images = [{} for _ in range(len(images_list))]
+
+    for idx, image in enumerate(images_list):
+        images[idx]['url'] = image
+        images[idx]['damage'] = labels[idx % 3]
+
+    return images
 
 if __name__ == '__main__':
     uvicorn.run("app:app", reload = True, host = '0.0.0.0', port=8081)
