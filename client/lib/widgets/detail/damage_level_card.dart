74067076c5515d2df88117876b0e37909cdd5b88
임두현 <dhlim07@naver.com>
2023-03-31 14:10:26 +0900

    feat : Update damage level card

--- client/lib/widgets/detail/damage_level_card.dart
+++ client/lib/widgets/detail/damage_level_card.dart
@@ -15,6 +15,9 @@ class DamageLevelCard extends StatefulWidget {
 }
 
 class _DamageLevelCardState extends State<DamageLevelCard> {
+  bool _isExpanded = false;
+
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -39,102 +42,171 @@ class _DamageLevelCardState extends State<DamageLevelCard> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 190,
-                        child: Text(
-                          widget.partName,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w500,
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    _isExpanded = !_isExpanded;
+                  });
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 190,
+                          child: Text(
+                            widget.partName,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          textAlign: TextAlign.center,
                         ),
-                      ),
-                      Stack(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(
-                              top: 10,
+                        Stack(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(
+                                top: 10,
+                              ),
+                              width: 180,
+                              height: 13,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: const Color(0xFFEFEFEF),
+                              ),
                             ),
-                            width: 180,
-                            height: 13,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              color: const Color(0xFFEFEFEF),
+                            Container(
+                              margin: const EdgeInsets.only(
+                                top: 10,
+                              ),
+                              width: 180 * (widget.damageLevel / 4),
+                              height: 13,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                  widget.damageLevel == 4
+                                    ? BorderRadius.circular(5)
+                                    : const BorderRadius.only(
+                                        topLeft: Radius.circular(5),
+                                        bottomLeft: Radius.circular(5),
+                                  ),
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 3
+                          ),
+                          child: Text(
+                            '${widget.damageLevel}',
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.w600
                             ),
                           ),
-                          Container(
-                            margin: const EdgeInsets.only(
-                              top: 10,
+                        ),
+                        const Text('건'),
+                        // const SizedBox(
+                        //   width: 10,
+                        // ),
+                        SizedBox(
+                          width: 40,
+                          child: Icon(
+                            !_isExpanded
+                              ? Icons.keyboard_arrow_down
+                              : Icons.keyboard_arrow_up,
+                            color: Theme.of(context).secondaryHeaderColor,
+                            // size: 40,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              if (_isExpanded)
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 30,
+                      ),
+                      child: Container(
+                        height: 1,
+                        width: double.infinity,
+                        color: const Color(0xFFEFEFEF),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        right: 10,
+                        left: 10,
+                        top: 20,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(7),
+                            child: Container(
+                              height: 70,
+                              width: 70,
+                              child: Image.network(
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtHU4BqZcNHPKDPWdn28TYFjpJWWNhZvUvWQ&usqp=CAU', // 이미지 URL
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                            width: 180 * (widget.damageLevel / 4),
-                            height: 13,
-                            decoration: BoxDecoration(
-                              borderRadius:
-                                widget.damageLevel == 4
-                                  ? BorderRadius.circular(5)
-                                  : const BorderRadius.only(
-                                      topLeft: Radius.circular(5),
-                                      bottomLeft: Radius.circular(5),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                  vertical: 3,
                                 ),
-                              color: Theme.of(context).primaryColor,
-                            ),
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColorLight,
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(7)
+                                  )
+                                ),
+                                child: Text(
+                                  '스크래치',
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w600
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '2023.03.03 18:30',
+                                style: TextStyle(
+                                  color: Theme.of(context).secondaryHeaderColor,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            right: 3
-                        ),
-                        child: Text(
-                          '${widget.damageLevel}',
-                          style: TextStyle(
-                              fontSize: 18,
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.w600
-                          ),
-                        ),
-                      ),
-                      const Text('건')
-                    ],
-                  ),
-
-                ],
-              ),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(7),
-                        child: Container(
-                          height: 50,
-                          width: 50,
-                          child: Image.network(
-                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtHU4BqZcNHPKDPWdn28TYFjpJWWNhZvUvWQ&usqp=CAU', // 이미지 URL
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 30,
-                      ),
-                      Text('2023.03.03 18:30'),
-                    ],
-                  ),
-                ],
-              )
+                    ),
+                  ],
+                )
             ],
           ),
         ),
