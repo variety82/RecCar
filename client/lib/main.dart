1678905a0b7b0a76d3e6cf34e24757f94c22ea3c
임두현 <dhlim07@naver.com>
2023-04-03 01:19:01 +0900

    refactor : Refactorying home

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,18 +1,17 @@
+import 'package:client/screens/splash_screen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-import 'widgets/common/footer.dart';
 import 'screens/register/car_register_main.dart';
 import 'screens/my_page/my_page.dart';
 import 'screens/map_screen/map_screen.dart';
 import 'screens/before_recording_screen/before_recording_screen.dart';
 import 'package:client/screens/video_recording_screen/video_recording_screen.dart';
 import 'screens/login_screen/login_screen.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'screens/detail/car_detail.dart';
 import 'screens/calendar_screen/calendar_screen.dart';
-import 'widgets/main_page/Main_Page_Body.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'screens/home/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +21,6 @@ void main() async {
   await dotenv.load(fileName: '.env');
   // 앱 처음 실행 시 flutter 엔진 초기화 메소드 호출
   // flutter 자체의 렌더링 엔진을 사용할 때 필요한 기본적인 설정들을 수행하는 메소드라고 생각하면 됨
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
       title: 'cilent',
@@ -35,9 +33,10 @@ void main() async {
         disabledColor: const Color(0xFF999999),
       ),
       themeMode: ThemeMode.system,
-      initialRoute: '/home',
+      initialRoute: '/',
       routes: {
-        '/home': (context) => const MyApp(),
+        '/' : (context) => const SplashScreen(),
+        '/home': (context) => const Home(),
         '/register': (context) => const CarRegister(),
         '/detail': (context) => const CarDetail(),
         '/my-page': (context) => const MyPage(),
@@ -51,110 +50,4 @@ void main() async {
   );
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  static const storage = FlutterSecureStorage();
-  dynamic userName = '';
-  dynamic userProfileImg = '';
-  dynamic userCarId = '0';
-
-  // dynamic userName = '';
-  // dynamic userEmail = '';
-
-  @override
-  void initState() {
-    super.initState();
-    // checkUserState();
-    // 비동기로 flutter secure storage 정보를 불러오는 작업
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      checkUserState();
-    });
-  }
-
-  checkUserState() async {
-    var name = await storage.read(key: 'nickName');
-    var img = await storage.read(key: 'picture');
-    var carId = await storage.read(key: 'carId');
-
-    setState(() {
-      userName = name;
-      userProfileImg = img;
-      userCarId = carId;
-    });
-    if (userName == null) {
-      Navigator.pushNamed(context, '/login'); // 로그인 페이지로 이동
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 100,
-          ),
-          Expanded(
-              child: userCarId == '0'
-                  ? MainPageBody(
-                      imgRoute: 'lib/assets/images/empty_garage.svg',
-                      imageDisabled: true,
-                      mainContainter: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '대여중인 차가 없습니다',
-                            style: TextStyle(
-                                fontSize: 18,
-                                color: Theme.of(context).primaryColor,
-                                fontWeight: FontWeight.w600),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            '자동차를 등록해주세요',
-                            style: TextStyle(
-                                color: Theme.of(context).secondaryHeaderColor,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                    )
-                  : MainPageBody(
-                      imgRoute: 'lib/assets/images/car_garage.svg',
-                      imageDisabled: false,
-                      mainContainter: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '초기 손상 확인하러 이동하기',
-                            style: TextStyle(
-                              color: Theme.of(context).secondaryHeaderColor
-                            ),
-                          ),
-                          ElevatedButton(
-                              onPressed: () {
-                                Navigator.pushNamed(context, '/detail');
-                              },
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                      Theme.of(context).primaryColor),
-                              child: const Text('차량 촬영'))
-                        ],
-                      ),
-                    ),
-          ),
-          const Footer()
-        ],
-      ),
-    );
-  }
-}
