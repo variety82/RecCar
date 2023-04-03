d6ed3ef7c5848b3096e09b4b93476b1301200e7a
unknown <owlwhnk@naver.com>
2023-04-03 11:32:42 +0900

    refactor: 오류 확인 및 일부 수정

--- client/lib/screens/home/home.dart
+++ client/lib/screens/home/home.dart
@@ -3,8 +3,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:client/widgets/common/footer.dart';
 import 'package:client/widgets/main_page/Main_Page_Body.dart';
 
-
-
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -17,6 +15,9 @@ class _HomeState extends State<Home> {
   dynamic userName = '';
   dynamic userProfileImg = '';
   dynamic userCarId = '0';
+  dynamic firstVideoInfo = true;
+  dynamic firstCheckDamage = true;
+  dynamic currentCarVideo = '0';
 
   // dynamic userName = '';
   // dynamic userEmail = '';
@@ -33,16 +34,19 @@ class _HomeState extends State<Home> {
 
   checkUserState() async {
     var name = await storage.read(key: 'nickName');
-    var img = await storage.read(key: 'picture');
+    var img = await storage.read(key: 'picture ');
     var carId = await storage.read(key: 'carId');
-
+    var videoinfo = await storage.read(key: 'firstVideoInfo');
+    var checkDamage = await storage.read(key: 'firstCheckDamage');
+    var carVideoState = await storage.read(key: 'carVideoState');
     setState(() {
       userName = name;
       userProfileImg = img;
       userCarId = carId;
+      firstVideoInfo = videoinfo;
+      firstCheckDamage = checkDamage;
+      currentCarVideo = carVideoState;
     });
-    print('1');
-    print(userCarId);
     if (userName == null) {
       Navigator.pushNamed(context, '/login'); // 로그인 페이지로 이동
     }
@@ -60,53 +64,47 @@ class _HomeState extends State<Home> {
           Expanded(
             child: userCarId == '0'
                 ? MainPageBody(
-              imgRoute: 'lib/assets/images/empty_garage.svg',
-              imageDisabled: true,
-              mainContainter: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '대여중인 차가 없습니다',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '자동차를 등록해주세요',
-                    style: TextStyle(
-                        color: Theme.of(context).secondaryHeaderColor,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ],
-              ),
-            )
+                    imgRoute: 'lib/assets/images/empty_garage.svg',
+                    imageDisabled: true,
+                    mainContainter: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '대여중인 차가 없습니다',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          '자동차를 등록해주세요',
+                          style: TextStyle(
+                              color: Theme.of(context).secondaryHeaderColor,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  )
                 : MainPageBody(
-              imgRoute: 'lib/assets/images/car_garage.svg',
-              imageDisabled: false,
-              mainContainter: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '초기 손상 확인하러 이동하기',
-                    style: TextStyle(
-                        color: Theme.of(context).secondaryHeaderColor
+                    imgRoute: 'lib/assets/images/car_garage.svg',
+                    imageDisabled: false,
+                    mainContainter: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/detail');
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Theme.of(context).primaryColor),
+                            child: const Text('차량 상세 페이지'))
+                      ],
                     ),
                   ),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/detail');
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                          Theme.of(context).primaryColor),
-                      child: const Text('차량 촬영'))
-                ],
-              ),
-            ),
           ),
           const Footer()
         ],
