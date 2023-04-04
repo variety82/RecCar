7822e2e02312364eb2999d19956e5fb509105515
임두현 <dhlim07@naver.com>
2023-04-04 17:41:04 +0900

    feat : Update modal action

--- client/lib/widgets/register/category_title.dart
+++ client/lib/widgets/register/category_title.dart
@@ -5,22 +5,32 @@ class CategoryTitle extends StatelessWidget {
 
   final String title;
   final bool isSelected;
+  final void Function(BuildContext, int?) showModal;
+  final int? modalIndex;
 
   const CategoryTitle({
-    super.key, required this.title, required this.isSelected,
+    super.key, required this.title, required this.isSelected, required this.showModal, this.modalIndex,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width : 80,
-      child: Text(
-        title,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 16,
-          color: isSelected ?  const Color(0xFF453F52) : const Color(0xFF999999),
-          fontWeight: isSelected ? FontWeight.w700 : FontWeight.w500
+    return InkWell(
+      onTap: modalIndex != null
+       ? () {
+        Navigator.pop(context);
+        showModal(context, modalIndex);
+      }
+      : () {},
+      child: SizedBox(
+        width : 80,
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 16,
+            color: isSelected ?  const Color(0xFF453F52) : const Color(0xFF999999),
+            fontWeight: isSelected ? FontWeight.w700 : FontWeight.w500
+          ),
         ),
       ),
     );
