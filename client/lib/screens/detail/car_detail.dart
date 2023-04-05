326a937677fa52787ab574195d4f0319d4cea72e
임두현 <dhlim07@naver.com>
2023-04-05 12:36:27 +0900

    feat : Update car detail overflow

--- client/lib/screens/detail/car_detail.dart
+++ client/lib/screens/detail/car_detail.dart
@@ -1,6 +1,5 @@
 import 'package:client/services/detail_api.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:client/screens/detail/part_detail.dart';
 import 'package:client/widgets/common/footer.dart';
@@ -14,8 +13,8 @@ class CarDetail extends StatefulWidget {
   State<CarDetail> createState() => _CarDetailState();
 }
 
-class _CarDetailState extends State<CarDetail> with SingleTickerProviderStateMixin {
-
+class _CarDetailState extends State<CarDetail>
+    with SingleTickerProviderStateMixin {
   FlutterSecureStorage storage = const FlutterSecureStorage();
 
   TabController? _tabController;
@@ -28,11 +27,14 @@ class _CarDetailState extends State<CarDetail> with SingleTickerProviderStateMix
     _tabController?.addListener(_handleTabSelection);
     _fetchCarInfo();
   }
+
   int _previousTabIndex = 0;
 
   void _handleTabSelection() {
     if (_tabController?.indexIsChanging ?? false) {
-      if (_tabController?.index == 1 && currentCarVideo == '1' && _previousTabIndex == 0) {
+      if (_tabController?.index == 1 &&
+          currentCarVideo == '1' &&
+          _previousTabIndex == 0) {
         _tabController?.animateTo(0);
         _showModal(context);
       }
@@ -46,7 +48,7 @@ class _CarDetailState extends State<CarDetail> with SingleTickerProviderStateMix
       builder: (BuildContext context) {
         return AlertDialog(
           content: Text(
-              '반납 영상이 등록되지 않았습니다.',
+            '반납 영상이 등록되지 않았습니다.',
             style: TextStyle(
               fontSize: 14,
               color: Theme.of(context).secondaryHeaderColor,
@@ -55,8 +57,9 @@ class _CarDetailState extends State<CarDetail> with SingleTickerProviderStateMix
           actions: <Widget>[
             ElevatedButton(
               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor),
-                minimumSize: MaterialStateProperty.all(const Size(60,35)),
+                backgroundColor:
+                    MaterialStateProperty.all(Theme.of(context).primaryColor),
+                minimumSize: MaterialStateProperty.all(const Size(60, 35)),
               ),
               onPressed: () {
                 Navigator.of(context).pop();
@@ -65,12 +68,13 @@ class _CarDetailState extends State<CarDetail> with SingleTickerProviderStateMix
             ),
             ElevatedButton(
               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor),
-                minimumSize: MaterialStateProperty.all(const Size(60,35)),
+                backgroundColor:
+                    MaterialStateProperty.all(Theme.of(context).primaryColor),
+                minimumSize: MaterialStateProperty.all(const Size(60, 35)),
               ),
               onPressed: () {
-                Navigator.pushNamed(
-                    context, '/before-recording');              },
+                Navigator.pushNamed(context, '/before-recording');
+              },
               child: const Text('등록'),
             ),
           ],
@@ -80,20 +84,24 @@ class _CarDetailState extends State<CarDetail> with SingleTickerProviderStateMix
   }
 
   Map<String, dynamic>? _detectionInfo;
+  Map<String, dynamic> _carInfo = {};
 
   Future<void> _fetchCarInfo() async {
-    getCarInfo(
-      success: (dynamic response) {
-        setState(() {
-          _detectionInfo = response;
-        });
-      },
-      fail: (error) {
-        print('차량 파손 정보 호출 오류 : $error');
-      }
-    );
+    getCarInfo(success: (dynamic response) {
+      setState(() {
+        _detectionInfo = response;
+        _carInfo = {
+          'carId': _detectionInfo?['carId'] ?? 0,
+          'carNumber': _detectionInfo?['carNumber'] ?? '',
+          'carManufacturer': _detectionInfo?['carManufacturer'] ?? '',
+          'carModel': _detectionInfo?['carModel'] ?? '',
+        };
+      });
+      // print(_detectionInfo);
+    }, fail: (error) {
+      print('차량 파손 정보 호출 오류 : $error');
+    });
   }
-
 
   @override
   void dispose() {
@@ -103,7 +111,8 @@ class _CarDetailState extends State<CarDetail> with SingleTickerProviderStateMix
 
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, String?>?;
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String?>?;
 
     if (args != null) {
       currentCarVideo = args['currentCarVideo'];
@@ -114,106 +123,122 @@ class _CarDetailState extends State<CarDetail> with SingleTickerProviderStateMix
       extendBodyBehindAppBar: true,
       // appBar:
       backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          Column(
-          children: [
-            SizedBox(
-              height: statusBarHeight,
-            ),
-            PreferredSize(
-              preferredSize: const Size.fromHeight(100),
-              child: TabBar(
-                  controller: _tabController,
-                  labelColor: Theme.of(context).secondaryHeaderColor,
-                  unselectedLabelColor: Theme.of(context).disabledColor,
-                  indicatorColor: Theme.of(context).primaryColor,
-                  indicatorWeight: 5,
-                  labelStyle: const TextStyle(fontWeight: FontWeight.w600),
-                  unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.w400),
-                  tabs: const [
-                    Tab(
-                      text: '대여',
-                    ),
-                    Tab(
-                      text: '반납',
-                    ),
-                  ]
+      body: _detectionInfo == null
+          ? Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor,
               ),
-            ),
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                physics: currentCarVideo == '1' ? const NeverScrollableScrollPhysics() : null,
-                children: [
-                  partDetail(
-                    detectionInfos: _detectionInfo?['initialDetectionInfos'] ?? {},
-                    frontDamageCount: _detectionInfo?['initialFrontDamageCount'] ?? 0,
-                    sideDamageCount: _detectionInfo?['initialSideDamageCount'] ?? 0,
-                    backDamageCount: _detectionInfo?['initialBackDamageCount'] ?? 0,
-                    wheelDamageCount: _detectionInfo?['initialWheelDamageCount'] ?? 0,
-                  ),
-                  partDetail(
-                    detectionInfos: _detectionInfo?['latterDetectionInfos'] ?? {},
-                    frontDamageCount: _detectionInfo?['latterFrontDamageCount'] ?? 0,
-                    sideDamageCount: _detectionInfo?['latterSideDamageCount'] ?? 0,
-                    backDamageCount: _detectionInfo?['latterBackDamageCount'] ?? 0,
-                    wheelDamageCount: _detectionInfo?['latterWheelDamageCount'] ?? 0,
-                  ),
-                ],
-              ),
-            ),
-            const Footer()
-            ],
-          ),
-          if (currentCarVideo == '2')
-            Positioned(
-              top: 120,
-              right: 20,
-              child: Stack(
-                clipBehavior: Clip.none,
-                alignment: AlignmentDirectional.topCenter,
-                children: [
-                  FloatingActionButton(
-                    onPressed: () {
-                      getCarReturn(
-                          success: (dynamic response) async {
-                            await storage.write(key: "carId", value: '0');
-                            await storage.write(key: "carVideoState", value: '0');
-                          },
-                          fail: (error) {
-                            print('차량 반납 요청 오류 : $error');
-                          }
-                      );
-                    },
-                    mini: true,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(16.0), // 원하는 모서리 반경을 설정합니다.
+            )
+          : Stack(
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: statusBarHeight,
+                    ),
+                    PreferredSize(
+                      preferredSize: const Size.fromHeight(100),
+                      child: TabBar(
+                          controller: _tabController,
+                          labelColor: Theme.of(context).secondaryHeaderColor,
+                          unselectedLabelColor: Theme.of(context).disabledColor,
+                          indicatorColor: Theme.of(context).primaryColor,
+                          indicatorWeight: 5,
+                          labelStyle:
+                              const TextStyle(fontWeight: FontWeight.w600),
+                          unselectedLabelStyle:
+                              const TextStyle(fontWeight: FontWeight.w400),
+                          tabs: const [
+                            Tab(
+                              text: '대여',
+                            ),
+                            Tab(
+                              text: '반납',
+                            ),
+                          ]),
+                    ),
+                    Expanded(
+                      child: TabBarView(
+                        controller: _tabController,
+                        physics: currentCarVideo == '1'
+                            ? const NeverScrollableScrollPhysics()
+                            : null,
+                        children: [
+                          partDetail(
+                            carInfo: _carInfo,
+                            detectionInfos:
+                                _detectionInfo?['initialDetectionInfos'] ?? {},
+                            frontDamageCount:
+                                _detectionInfo?['initialFrontDamageCount'] ?? 0,
+                            sideDamageCount:
+                                _detectionInfo?['initialMidDamageCount'] ?? 0,
+                            backDamageCount:
+                                _detectionInfo?['initialBackDamageCount'] ?? 0,
+                            wheelDamageCount:
+                                _detectionInfo?['initialWheelDamageCount'] ?? 0,
+                          ),
+                          partDetail(
+                            carInfo: _carInfo,
+                            detectionInfos:
+                                _detectionInfo?['latterDetectionInfos'] ?? {},
+                            frontDamageCount:
+                                _detectionInfo?['latterFrontDamageCount'] ?? 0,
+                            sideDamageCount:
+                                _detectionInfo?['latterMidDamageCount'] ?? 0,
+                            backDamageCount:
+                                _detectionInfo?['latterBackDamageCount'] ?? 0,
+                            wheelDamageCount:
+                                _detectionInfo?['latterWheelDamageCount'] ?? 0,
+                          ),
+                        ],
                       ),
                     ),
-                    backgroundColor: Theme.of(context).primaryColor,
-                    child: const Icon(
-                        Icons.reply,
-                    ),
-                  ),
+                    const Footer()
+                  ],
+                ),
+                if (currentCarVideo == '2')
                   Positioned(
-                    top: 55,
-                    child: Text(
-                        '반납하기',
-                      style: TextStyle(
-                        color: Theme.of(context).secondaryHeaderColor,
-                        fontWeight: FontWeight.w500
-                      ),
+                    top: 120,
+                    right: 20,
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      alignment: AlignmentDirectional.topCenter,
+                      children: [
+                        FloatingActionButton(
+                          onPressed: () {
+                            getCarReturn(success: (dynamic response) async {
+                              await storage.write(key: "carId", value: '0');
+                              await storage.write(
+                                  key: "carVideoState", value: '0');
+                            }, fail: (error) {
+                              print('차량 반납 요청 오류 : $error');
+                            });
+                          },
+                          mini: true,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(16.0), // 원하는 모서리 반경을 설정합니다.
+                            ),
+                          ),
+                          backgroundColor: Theme.of(context).primaryColor,
+                          child: const Icon(
+                            Icons.reply,
+                          ),
+                        ),
+                        Positioned(
+                          top: 55,
+                          child: Text(
+                            '반납하기',
+                            style: TextStyle(
+                                color: Theme.of(context).secondaryHeaderColor,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
+              ],
             ),
-        ],
-      ),
     );
   }
 }
-
-
