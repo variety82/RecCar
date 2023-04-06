bcedb565996541a4ce375205a50088bbc3f60421
changhyunKim <ch03124@naver.com>
2023-04-07 08:21:17 +0900

    feat : 접근 권한 수정

--- APP/app.py
+++ APP/app.py
@@ -34,33 +34,37 @@ def startup_event():
 
 @app.post("/ai-api/v1/damage", response_model=List[schema.InferenceImage]) 
 async def upload_video(file : UploadFile, user_id : str):
-    upload_path = "./dataset/video"
-    content = await file.read()
-    now = str(datetime.datetime.now()).replace(" ", "_").replace(":", "-")[:-7]
+    if(user_id == "RecCar" or user_id == "TAEKYUN"):
+        upload_path = "./dataset/video"
+        content = await file.read()
+        now = str(datetime.datetime.now()).replace(" ", "_").replace(":", "-")[:-7]
 
-    filen_name = f"user_{user_id}_{now}.mp4"
-    with open(os.path.join(upload_path, filen_name), "wb") as fp:
-        fp.write(content)  # 서버 로컬 스토리지에 이미지 저장 (쓰기)
-    
-    images = video_to_image(filen_name, 30)
+        filen_name = f"user_{user_id}_{now}.mp4"
+        with open(os.path.join(upload_path, filen_name), "wb") as fp:
+            fp.write(content)  # 서버 로컬 스토리지에 이미지 저장 (쓰기)
+        
+        images = video_to_image(filen_name, 30)
 
-    created_img = inference.create_images(models, images)
+        created_img = inference.create_images(models, images)
 
-    obj_list = upload_s3(s3, bucket, created_img, "hero")
-    images_list = get_images_url(s3, bucket, obj_list)
+        obj_list = upload_s3(s3, bucket, created_img, "hero")
+        images_list = get_images_url(s3, bucket, obj_list)
 
-    images = [{} for _ in range(len(images_list))]
+        images = [{} for _ in range(len(images_list))]
 
-    for idx, image in enumerate(images_list):
-        images[idx]['url'] = image
+        for idx, image in enumerate(images_list):
+            images[idx]['url'] = image
 
-    return images
+        return images
+    else:
+        return "http://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg"
 
 @app.post("/ai-api/v1/image")
 async def upload_image(file : UploadFile, user_id : str):
+    
     upload_path = "./dataset/images"
     content = await file.read()
-    file_name = f"user_{user_id}.jpg"
+    file_name = f"{user_id}.jpg"
 
     with open(os.path.join(upload_path, file_name), "wb") as fp:
         fp.write(content)  # 서버 로컬 스토리지에 이미지 저장 (쓰기)
